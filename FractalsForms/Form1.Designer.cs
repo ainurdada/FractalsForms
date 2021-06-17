c5b86f96e42f699865fd0de781603a1fddc04966
ainurdada <ainurdada@gmail.com>
2021-06-17 17:35:50 +0300

    first commit

---
+++ FractalsForms/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace FractalsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Fractal";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FractalsForms/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FractalsForms
+{
+    public partial class Form1 : Form
+    {
+        int x = 0;
+        double zoom = 1;
+        double offsetX = 0, offsetY = 0;
+        public Form1()
+        {
+
+            InitializeComponent();
+
+
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+
+            System.Windows.Forms.Timer timer = new Timer();
+            timer.Interval = 1;
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Start();
+
+            this.MouseWheel += new MouseEventHandler(this_MouseWheel);
+            this.KeyDown += new KeyEventHandler(this_KeyDown);
+        }
+
+        private void this_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.A)
+            {
+                offsetX += -10 / zoom;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                offsetX += 10 / zoom;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                offsetY += 10 / zoom;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                offsetY += -10 / zoom;
+            }
+
+            x = 0;
+        }
+
+        private void this_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if(e.Delta > 0)
+            {
+                zoom += 0.1;
+                x = 0;
+
+            }
+            else 
+            {
+                if (zoom > 1)
+                {
+                    zoom -= 0.1;
+                    x = 0;
+                }
+                else
+                {
+                    zoom = 1;
+                }
+            }
+        }
+
+
+        //minr - min Real number
+        //maxr - max Real number
+        //mini - min Im number
+        //maxi - max Im number
+        static Bitmap FractalMandelbrod(PictureBox pictureBox, Bitmap image, int x, double minr, double maxr, double mini, double maxi, double zoom, double offsetX, double offsetY)
+        {
+
+            double zx = 0, zy = 0, cx = 0, cy = 0;
+
+
+            //multiply that convert pixel coords to plane coord
+            double xMultiply = (maxr - minr) / Convert.ToDouble(image.Width);
+            double yMultiply = (maxi - mini) / Convert.ToDouble(image.Height);
+
+            //max iterations for one point
+            int maxIterations = 50;
+
+            double zoomedWidth = pictureBox.Width / zoom;
+            double zoomedHeight = pictureBox.Height / zoom;
+            double minZoomPointX = (pictureBox.Width - zoomedWidth) / 2;
+            double minZoomPointY = (pictureBox.Height - zoomedHeight) / 2;
+
+
+            cx = (x * (zoomedWidth / pictureBox.Width) + minZoomPointX + offsetX) * xMultiply - Math.Abs(minr);
+            for (int y = 0; y < pictureBox.Height; y++)
+            {
+                zx = 0;
+                zy = 0;
+                cy = (y * (zoomedHeight / pictureBox.Height) + minZoomPointY + offsetY) * yMultiply - Math.Abs(mini);
+                int iteration = 0;
+                while (zx * zx + zy * zy <= 4 && iteration < maxIterations)
+                {
+                    iteration++;
+                    double tempZX = zx;
+                    zx = Math.Pow(zx, 2) - Math.Pow(zy, 2) + cx;
+                    zy = 2 * tempZX * zy + cy;
+                }
+
+                image.SetPixel(x, y, Color.FromArgb((iteration % 255), (iteration % 85) * 3, (iteration % 128) * 2));
+            }
+            return image;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            for (int i = x; i< pictureBox1.Width && i <= x + 10; i++)
+            {
+                pictureBox1.Image = FractalMandelbrod(pictureBox1, (Bitmap)pictureBox1.Image, i, -2, 2, -2, 2, zoom, offsetX, offsetY);
+                if(i == x + 4)
+                {
+                    x = i;
+                    break;
+                }
+            }
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            ////Bitmap image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //for (int x = 0; x < pictureBox1.Width; x++)
+            //{
+            //    pictureBox1.Image = FractalMandelbrod(pictureBox1, (Bitmap)pictureBox1.Image, x, -2, 2, -2, 2, 5);
+            //}
+        }
+    }
+}
