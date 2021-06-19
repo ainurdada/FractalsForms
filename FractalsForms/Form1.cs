f5d44a640cedccc2872a1ae718636c39b31521fd
ainurdada <ainurdada@gmail.com>
2021-06-19 11:43:14 +0300

    mini optimization

--- FractalsForms/Form1.cs
+++ FractalsForms/Form1.cs
@@ -12,9 +12,9 @@ namespace FractalsForms
 {
     public partial class Form1 : Form
     {
-        int x = 0;
         double zoom = 1;
         double offsetX = 0, offsetY = 0;
+        int smoothStep = 10, maxSmooth = 50, currentSmooth = 0;
         public Form1()
         {
 
@@ -31,11 +31,13 @@ namespace FractalsForms
 
             this.MouseWheel += new MouseEventHandler(this_MouseWheel);
             this.KeyDown += new KeyEventHandler(this_KeyDown);
+
+            currentSmooth = smoothStep;
         }
 
         private void this_KeyDown(object sender, KeyEventArgs e)
         {
-            if(e.KeyCode == Keys.A)
+            if (e.KeyCode == Keys.A)
             {
                 offsetX += -10 / zoom;
             }
@@ -52,23 +54,23 @@ namespace FractalsForms
                 offsetY += -10 / zoom;
             }
 
-            x = 0;
+            currentSmooth = smoothStep;
         }
 
         private void this_MouseWheel(object sender, MouseEventArgs e)
         {
-            if(e.Delta > 0)
+            if (e.Delta > 0)
             {
                 zoom += 0.1;
-                x = 0;
+                currentSmooth = smoothStep;
 
             }
-            else 
+            else
             {
                 if (zoom > 1)
                 {
                     zoom -= 0.1;
-                    x = 0;
+                    currentSmooth = smoothStep;
                 }
                 else
                 {
@@ -82,7 +84,7 @@ namespace FractalsForms
         //maxr - max Real number
         //mini - min Im number
         //maxi - max Im number
-        static Bitmap FractalMandelbrod(PictureBox pictureBox, Bitmap image, int x, double minr, double maxr, double mini, double maxi, double zoom, double offsetX, double offsetY)
+        static Bitmap FractalMandelbrod(PictureBox pictureBox, Bitmap image, double minr, double maxr, double mini, double maxi, double zoom, double offsetX, double offsetY, int maxIterations, double power)
         {
 
             double zx = 0, zy = 0, cx = 0, cy = 0;
@@ -92,44 +94,40 @@ namespace FractalsForms
             double xMultiply = (maxr - minr) / Convert.ToDouble(image.Width);
             double yMultiply = (maxi - mini) / Convert.ToDouble(image.Height);
 
-            //max iterations for one point
-            int maxIterations = 50;
 
             double zoomedWidth = pictureBox.Width / zoom;
             double zoomedHeight = pictureBox.Height / zoom;
             double minZoomPointX = (pictureBox.Width - zoomedWidth) / 2;
             double minZoomPointY = (pictureBox.Height - zoomedHeight) / 2;
 
-
-            cx = (x * (zoomedWidth / pictureBox.Width) + minZoomPointX + offsetX) * xMultiply - Math.Abs(minr);
-            for (int y = 0; y < pictureBox.Height; y++)
+            for (int x = 0; x < pictureBox.Width; x++)
             {
-                zx = 0;
-                zy = 0;
-                cy = (y * (zoomedHeight / pictureBox.Height) + minZoomPointY + offsetY) * yMultiply - Math.Abs(mini);
-                int iteration = 0;
-                while (zx * zx + zy * zy <= 4 && iteration < maxIterations)
+                cx = (x * (zoomedWidth / pictureBox.Width) + minZoomPointX + offsetX) * xMultiply - Math.Abs(minr);
+                for (int y = 0; y < pictureBox.Height; y++)
                 {
-                    iteration++;
-                    double tempZX = zx;
-                    zx = Math.Pow(zx, 2) - Math.Pow(zy, 2) + cx;
-                    zy = 2 * tempZX * zy + cy;
-                }
+                    zx = 0;
+                    zy = 0;
+                    cy = (y * (zoomedHeight / pictureBox.Height) + minZoomPointY + offsetY) * yMultiply - Math.Abs(mini);
+                    int iteration = 0;
+                    while (zx * zx + zy * zy <= 4 && iteration < maxIterations)
+                    {
+                        iteration++;
+                        double tempZX = zx;
+                        zx = Math.Pow(zx, power) - Math.Pow(zy, power) + cx;
+                        zy = power * tempZX * zy + cy;
+                    }
 
-                image.SetPixel(x, y, Color.FromArgb((iteration % 255), (iteration % 85) * 3, (iteration % 128) * 2));
+                    image.SetPixel(x, y, Color.FromArgb((iteration % 50) * 5, (iteration % 85) * 3, (iteration % 128) * 2));
+                }
             }
             return image;
         }
         private void timer_Tick(object sender, EventArgs e)
         {
-            for (int i = x; i< pictureBox1.Width && i <= x + 10; i++)
+            if (currentSmooth <= maxSmooth)
             {
-                pictureBox1.Image = FractalMandelbrod(pictureBox1, (Bitmap)pictureBox1.Image, i, -2, 2, -2, 2, zoom, offsetX, offsetY);
-                if(i == x + 4)
-                {
-                    x = i;
-                    break;
-                }
+                pictureBox1.Image = FractalMandelbrod(pictureBox1, (Bitmap)pictureBox1.Image, -2, 2, -2, 2, zoom, offsetX, offsetY, currentSmooth, 2);
+                currentSmooth += smoothStep;
             }
         }
         private void pictureBox1_Click(object sender, EventArgs e)
